f8ca49fd1b3ac0cbb23acbd51cbb358888fdbbfb
abedirahan <abedirhan@hotmail.com>
2019-06-29 18:11:55 -0400

    BMi first copy 2109-06-29

--- Assignment4BMI/Form1.cs
+++ Assignment4BMI/Form1.cs
@@ -16,5 +16,11 @@ namespace Assignment4BMI
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+               
+        }
     }
 }
